7f51adb09d62e75db159eda6c040c9eef2eddf2b
kyeongbok <kyeongbok_0627@kakao.com>
2025-06-26 16:27:51 +0900

    202506261627

---
+++ src/src/SopPage.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Worker, Viewer } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+
+const SopPage: React.FC = () => {
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
+  return (
+    <div style={{ height: '100vh', width: '100%' }}>
+      <Worker workerUrl={`https://unpkg.com/pdfjs-dist@2.16.105/build/pdf.worker.min.js`}>
+        <Viewer
+          fileUrl="/assets/Semicon.pdf" // public/assets/Semicon.pdf 위치
+          plugins={[defaultLayoutPluginInstance]}
+        />
+      </Worker>
+    </div>
+  );
+};
+
+export default SopPage;
